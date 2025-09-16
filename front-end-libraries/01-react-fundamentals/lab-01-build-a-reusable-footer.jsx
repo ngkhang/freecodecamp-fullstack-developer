df254a0cf0fdb24d6465a38401150a782f407c9b
Khang Nguyen <ngkhang0220@gmail.com>
2025-09-16 10:16:00 +0700

    add: react fundamentals - lab and quiz solutions

---
+++ front-end-libraries/01-react-fundamentals/lab-01-build-a-reusable-footer.jsx
@@ -0,0 +1,25 @@
+export const Footer = () => {
+	return (
+		<footer>
+			<ul>
+				<li>Fitness Dashboard</li>
+				<li>Services</li>
+			</ul>
+			<ul>
+				<li>Watch Videos</li>
+				<li>Discord</li>
+			</ul>
+			<ul>
+				<li>Privacy Policy</li>
+				<li>Terms & Conditions</li>
+			</ul>
+
+			<p>© 2024 Fitness Dashboard. All Rights Reserved.</p>
+
+			<a href="#">🎮</a>
+			<a href="#">🐦</a>
+			<a href="#">💻</a>
+			<a href="#">🏀</a>
+		</footer>
+	);
+};
